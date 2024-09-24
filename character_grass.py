a5fe4473c513db885b47323b68211b5f06610ff7
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-09-24 12:44:09 +0900

    캐릭터가 캔버스 밖으로 안나가게 하기

--- character_grass.py
+++ character_grass.py
@@ -12,7 +12,7 @@ def draw_character(x, y):
     delay(0.01)
 
 def run_circle():
-    r, cx, cy = 300, 800 // 2, 600 // 2
+    r, cx, cy = 250, 800 // 2, 600 // 2
 
     for degree in range(0, 360, 3):
         theta = math.radians(degree)
@@ -22,20 +22,20 @@ def run_circle():
         draw_character(x, y)
         
 def run_top():
-    for x in range(50, 750, 10):
+    for x in range(20, 780, 10):
         draw_character(x, 550)
 
 def run_right():
     for y in range(550, 50, -10):
-        draw_character(750, y)
+        draw_character(780, y)
 
 def run_bottom():
-    for x in range(750, 50, -10):
+    for x in range(780, 20, -10):
         draw_character(x, 50)
 
 def run_left():
     for y in range(50, 550, 10):
-        draw_character(50, y)
+        draw_character(20, y)
    
 def run_rectangle():
     run_top()
@@ -44,9 +44,8 @@ def run_rectangle():
     run_left()
 
 while True:
-    #run_circle()
+    run_circle()
     run_rectangle()
-    break;
 
 close_canvas()
 
